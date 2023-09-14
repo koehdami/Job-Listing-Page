9934c5576642b842c6c9fa5922bc19c8978080ea
koehdami <damiankoehler2612@gmailcom>
2023-09-14 18:55:52 +0200

    added basic unit-testing

--- main/models.py
+++ main/models.py
@@ -9,13 +9,22 @@ class user_data(models.Model):
     job_title = models.CharField(max_length=64, default="")
     phone_number = models.CharField(max_length=32, default="")
 
+    def __str__(self):
+        return f"{self.name} is working at {self.current_job} as a {self.job_title} Phonenumber:{self.phone_number}"
+
 class categories(models.Model):
     id = models.IntegerField(primary_key=True, unique=True),
     name = models.CharField(max_length=64)
 
+    def __str__(self):
+        return f"Name:{self.name}, Id:{self.id}"
+
 class languages(models.Model):
     id = models.IntegerField(primary_key=True, unique=True),
     name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"Name:{self.name}, Id:{self.id}"
 
 
 class job_offers(models.Model):
@@ -30,11 +39,20 @@ class job_offers(models.Model):
     post_date = models.DateTimeField(auto_now=True)
     part_time = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f"Job {self.id} with title {self.title} from {self.user}"
+
 class applications(models.Model):
     id = models.IntegerField(primary_key=True, unique=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     job_offer_id = models.ForeignKey(job_offers, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"Id:{self.id} User {self.user_id} applied to {self.job_offer_id}"
+    
+    def valid_user(self):
+        return self.user_id != self.job_offer_id.user
+    
 class contactRequests(models.Model):
     id = models.IntegerField(primary_key=True, unique=True)
     first_name = models.TextField(max_length=64)
@@ -42,3 +60,9 @@ class contactRequests(models.Model):
     email = models.EmailField()
     phone_number = models.TextField(max_length=32)
     content = models.CharField(max_length=512)
+
+    def __str__(self):
+        return f"{self.first_name}  {self.last_name}, requestedID: {self.id}"
+    
+    def valid_request(self):
+        return self.first_name != self.last_name
--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,128 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
+from django.contrib.auth.models import User
+from .models import user_data, categories, job_offers, languages, contactRequests, applications
 # Create your tests here.
+
+class Tests(TestCase):
+
+    def setUp(self):
+
+        testCategory = categories.objects.create(name="testCategory")
+        testCategory2 = categories.objects.create(name="testCategory2")
+
+        testLanguage = languages.objects.create(name="testLanguage")
+        testLanguage2 = languages.objects.create(name="testLanguage2")
+
+        testUser = User.objects.create_user(
+                    username="testUsername", 
+                    email="test@example.com", 
+                    password="123Password", 
+                    first_name="testFirstName", 
+                    last_name="testLastName")
+        
+        testUser2 = User.objects.create_user(
+                username="testUsername2", 
+                email="test2@example.com", 
+                password="123Password", 
+                first_name="testFirstName2", 
+                last_name="testLastName2")
+
+
+        testJobListing = job_offers.objects.create(
+            user = testUser,
+            title = "Test job title",
+            category = testCategory,
+            pay = "1234€",
+            languages = testLanguage,
+            company = "testCompany",
+            description = "testDescription",
+        )
+
+        testApplication = applications.objects.create(
+            user_id = testUser2, 
+            job_offer_id = testJobListing
+            )
+        
+        testApplication2 = applications.objects.create(
+            user_id = testUser, 
+            job_offer_id = testJobListing
+        )
+        
+        testContactRequest = contactRequests.objects.create(
+            first_name = "testFirstname",
+            last_name = "testLastname",
+            email = "test@example.com",
+            phone_number = "00001111111",
+            content = "Test text for the test contactRequest"
+        )
+
+
+    def test_categories_count_all(self):
+        c = categories.objects.all()
+        self.assertEqual(c.count(), 2)
+
+    def test_languages_count_all(self):
+        c = languages.objects.all()
+        self.assertEqual(c.count(), 2)
+
+    def test_valid_application(self):
+        u = User.objects.get(username="testUsername2")
+        a = applications.objects.get(user_id = u)
+        self.assertTrue(a.valid_user())
+
+    def test_valid_application_false(self):
+        u = User.objects.get(username="testUsername")
+        a = applications.objects.get(user_id = u)
+        self.assertFalse(a.valid_user())
+
+    def test_valid_contact(self):
+        c = contactRequests.objects.get(first_name = "testFirstname")
+        self.assertTrue(c.valid_request())
+
+
+
+    def test_index(self):
+        c = Client()
+        response = c.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_jobs(self):
+        c = Client()
+        response = c.get("/jobs")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["jobs"].count(), 1)
+
+    def test_contact(self):
+        c = Client()
+        response = c.get("/contact")
+        self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        c = Client()
+        response = c.get("/login")
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout(self):
+        c = Client()
+        response = c.get("/logout")
+        self.assertEqual(response.status_code, 200)
+            
+    def test_register(self):
+        c = Client()
+        response = c.get("/register")
+        self.assertEqual(response.status_code, 200)
+
+    def test_specific_job(self):
+        testUser = User.objects.get(username="testUsername")
+        testJob = job_offers.objects.get(user_id = testUser)
+
+        c = Client()
+        response = c.get(f"/jobs/{testJob.id}")
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_specific_id(self):
+        max_id = job_offers.objects.all().count()
+        c = Client()
+        response = c.get(f"/jobs/{max_id + 1}")
+        self.assertEqual(response.context["message"], "Error, couldn't find job. Please try agian.")
